755911cea51a45fb645789bbfef1ae0a06ce5efa
@momviseuite <momviseuite@gmail.com>
2025-03-06 23:03:31 +0700

    2

---
+++ src/app/pages/admin/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginObj: any = {
+    username: '',
+    password: ''
+  };
+
+  constructor(private router: Router){}
+
+  onLogin(){
+    if(this.loginObj.username == "admin" && this.loginObj.password == "1234"){
+
+      this.router.navigateByUrl('/products')
+
+    }else{
+      alert('Wrong Credentials')
+    }
+  }
+
+
+}
